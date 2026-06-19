645ff42352f4775bf12d0eaf91589def1ab81e44
Gabriel-V-Maia <gabriel_v_maia@estudante.sesisenai.org.br>
2026-06-19 16:14:52 -0300

    feat(rssreader): add validation to incoming files and maextract relevant info

--- src/modules/rssreader/rssreader.go
+++ src/modules/rssreader/rssreader.go
@@ -1,11 +1,31 @@
 package rssreader
 
 import (
+	"bytes"
 	"fmt"
 	"io"
 	"net/http"
 	"os"
+	"regexp"
 )
+
+func ParseRSS(file *os.File) ([]string, error) {
+	content, err := io.ReadAll(file)
+	if err != nil {
+		return nil, fmt.Errorf("error reading file: %w", err)
+	}
+
+	re := regexp.MustCompile(`<!\[CDATA\[(.*?)\]\]>`)
+	matches := re.FindAllStringSubmatch(string(content), -1)
+
+	var results []string
+
+	for _, match := range matches {
+		results = append(results, match[1])
+	}
+
+	return results, nil
+}
 
 func GetRSS(url string, filename string) (*os.File, error) {
 	res, err := http.Get(url)
@@ -16,19 +36,37 @@ func GetRSS(url string, filename string) (*os.File, error) {
 
 	defer res.Body.Close()
 
+	buffer := make([]byte, 42)
+
+	_, err = io.ReadFull(res.Body, buffer)
+
+	if err != nil {
+		return nil, err
+	}
+
+	signature := "<?xml version=\"1.0\" encoding=\"UTF-8\"?><rss"
+
+	if string(buffer) != signature {
+		return nil, fmt.Errorf(
+			"Invalid RSS signature, received: %s\n expected %s",
+			buffer, signature)
+	}
+
 	file, err := os.Create(filename)
 
 	if err != nil {
+		file.Close()
 		return nil, err
 	}
 
-	defer file.Close()
+	fullReader := io.MultiReader(bytes.NewReader(buffer), res.Body)
 
-	_, err = io.Copy(file, res.Body)
-
+	_, err = io.Copy(file, fullReader)
 	if err != nil {
 		return nil, err
 	}
+
+	file.Seek(0, 0)
 
 	return file, nil
 }
