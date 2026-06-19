f286ebee7e8cc2015123e682e240f5993f184f26
Gabriel-V-Maia <gabriel_v_maia@estudante.sesisenai.org.br>
2026-06-19 13:20:08 -0300

    feat: makes it so the bot actually connects to discord

--- src/main.go
+++ src/main.go
@@ -5,10 +5,29 @@ import (
 	"os"
 
 	"darkwebinformer-bot/src/modules/envreader"
+	"github.com/bwmarrin/discordgo"
 )
 
 func main() {
 	envreader.LoadEnv(".env")
 
-	fmt.Println(os.Getenv("TOKEN"))
+	token := os.Getenv("TOKEN")
+
+	dg, err := discordgo.New("Bot " + token)
+
+	if err != nil {
+		fmt.Println("Error creating discord session", err)
+	}
+
+	dg.AddHandler(func(s *discordgo.Session, r *discordgo.Ready) {
+		fmt.Printf("Logged on as %s\n", s.State.User.Username)
+	})
+
+	err = dg.Open()
+
+	if err != nil {
+		fmt.Println("Error opening websocket to discord: ", err)
+	}
+
+	select {}
 }
