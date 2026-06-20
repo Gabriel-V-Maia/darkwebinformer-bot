29e8e939c5d8448294c7f572cb6fb3abcdf7a3ff
Gabriel-V-Maia <gabriel_v_maia@estudante.sesisenai.org.br>
2026-06-20 16:17:01 -0300

    refactor: makes it so SendEmbed gets a list of posts and finds target companies by post patterns

--- src/modules/notifier/notifier.go
+++ src/modules/notifier/notifier.go
@@ -1,31 +1,57 @@
 package notifier
 
 import (
+	"strings"
+
+	"darkwebinformer-bot/src/modules/rssreader"
+
 	"github.com/bwmarrin/discordgo"
 )
 
-func SendEmbed(session *discordgo.Session) {
+func SendEmbed(session *discordgo.Session, channelID string, item rssreader.Item) {
+	targetCompany := "Não especificada"
+	if strings.Contains(item.Title, "From ") {
+		parts := strings.Split(item.Title, "From ")
+		if len(parts) > 1 {
+			targetCompany = parts[1]
+		}
+	} else if strings.Contains(item.Title, "Allegedly Breached") {
+		parts := strings.Split(item.Title, " Allegedly")
+		targetCompany = parts[0]
+	}
+
+	desc := item.Description
+	if len(desc) > 1000 {
+		desc = desc[:997] + "..."
+	}
+
 	embed := &discordgo.MessageEmbed{
-		Author:      &discordgo.MessageEmbedAuthor{},
+		Title:       item.Title,
+		URL:         item.Link,
 		Color:       0x99ffee,
-		Description: "{SEÇÃO}",
+		Description: desc,
 		Fields: []*discordgo.MessageEmbedField{
-			&discordgo.MessageEmbedField{
-				Name:   "Tópico",
-				Value:  "I am a value",
+			{
+				Name:   "Tópico / Categoria",
+				Value:  item.Category,
 				Inline: true,
 			},
-			&discordgo.MessageEmbedField{
-				Name:   "Empresa alvo",
-				Value:  "I am a value",
+			{
+				Name:   "Empresa Alvo",
+				Value:  targetCompany,
 				Inline: true,
 			},
 		},
 		Thumbnail: &discordgo.MessageEmbedThumbnail{
 			URL: "https://cdn.discordapp.com/avatars/119249192806776836/cc32c5c3ee602e1fe252f9f595f9010e.jpg?size=2048",
 		},
-		Title: "I am an Embed",
 	}
 
-	session.ChannelMessageSendEmbed("999", embed)
+	if item.PubDate != "" {
+		embed.Footer = &discordgo.MessageEmbedFooter{
+			Text: "Publicado: " + item.PubDate,
+		}
+	}
+
+	session.ChannelMessageSendEmbed(channelID, embed)
 }
